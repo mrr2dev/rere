448e8357564c5dd77c12afd84c59d7f0c839751b
Amatir_iam <122685493+mrr2dev@users.noreply.github.com>
2023-03-13 05:59:21 +0700

    Update Al-Ikhlas-Contract.ts

--- consts/Al-Ikhlas-Contract.ts
+++ consts/Al-Ikhlas-Contract.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
-  "0x40811529234f1782Bea996f1D8c89631511b38D7";
+  "0x8cEc2Ab5Bf43C231a010fAa30DD7Ab86745d3bb8";
 export const tokenContractAddress =
-  "0x1480A5dc8DC864E671023DdD65e8B1fb41262EB8";
+  "0x4a3d6045952167De9A3803Bf06ae2eFe7591C2a8";
 export const stakingContractAddress =
-  "0x70c6C09Ec5b7f67244dB68481Bee4685a12c5d12";
+  "0x9F126fb257fa37C575D24aFDf7EB3d31140daC95";
