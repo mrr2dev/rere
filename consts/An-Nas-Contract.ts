bc9a51419f1e6af648077322f5e80cea1618bd94
Amatir_iam <122685493+mrr2dev@users.noreply.github.com>
2023-03-13 05:30:14 +0700

    Update An-Nas-Contract.ts

--- consts/An-Nas-Contract.ts
+++ consts/An-Nas-Contract.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
   "0x5cD1C3C0e607178339185fa75b11F9A184531d5C";
 export const tokenContractAddress =
-  "0x1480A5dc8DC864E671023DdD65e8B1fb41262EB8";
+  "0x4a3d6045952167De9A3803Bf06ae2eFe7591C2a8";
 export const stakingContractAddress =
-  "0x70c6C09Ec5b7f67244dB68481Bee4685a12c5d12";
+  "0xE223334D1F7904DFa0dd3fd6E48b10EFeeE71f3A";
