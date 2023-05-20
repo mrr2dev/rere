18f8b00b1c3f449ce64f6f01225393bd3391b2dc
Amatir_iam <122685493+mrr2dev@users.noreply.github.com>
2023-05-20 16:18:45 +0700

    Create Sheikh-Zayed-Grand-Mosque-Contract.ts

---
+++ consts/consts/Sheikh-Zayed-Grand-Mosque-Contract.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0xcaB7AE3150eAffe5D962027D766f3dbE072c1fc0";
+export const tokenContractAddress =
+  "0x4a3d6045952167De9A3803Bf06ae2eFe7591C2a8";
+export const stakingContractAddress =
+  "0x2cc0c0d9C479bE29c4Ee950db88164832F98405c";
